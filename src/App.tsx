78c92771868441b61aadcee1dbb35ad78c60fe5a
troykort <troykort@gmail.com>
2023-12-14 13:43:59 +0100

    Routing little bit

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,21 @@
 import { useState } from "react";
 import LoginPage from './Components/LoginPage/LoginPage/Login'
 import ErrorPage from './Components/ErrorPage/Error'
-import { Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 
 function App() {
-  
-
-  return (
-    
-    <div>
-      
-          <LoginPage />
-
-    </div>
-    
+  return ( 
+    <Router>
+      <div>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        {/* <Route path="/error" element={<ErrorPage />} /> */}
+      </Routes>
+      </div>
+    </Router>
   );
 }
 
 
-export default App;
-// import React from 'react';
-// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-// // import LoginPage from './pages/LoginPage';
-// // import HomePage from './pages/HomePage';
-// import Homep from './Components/HomePage/HomePage'
-// const App: React.FC = () => {
-//   return (
-//     <Router>
-//       <Switch>
-//         <Route path="/" exact component={LoginPage} />
-//         <Route path="/home" component={Homep} />
-//         {/* Add more routes for other pages as needed */}
-//       </Switch>
-//     </Router>
-//   );
-// };
-
-// export default App;
+export default App;
