715d8325647ce2cb9df0cc5e6a8b260da0fd6952
troykort <troykort@gmail.com>
2024-02-03 00:41:31 +0100

    works again

--- Server/obj/Debug/net8.0/server.AssemblyInfo.cs
+++ Server/obj/Debug/net8.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7218b3af464ffcaec85fe667ca3a3c2a61756430")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f958db8b750c4835980992e46bd0c539493fef9e")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
