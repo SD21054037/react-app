b6b03451532831fb38b70996a97cc28ed42e8deb
troykort <troykort@gmail.com>
2024-02-02 18:41:34 +0100

    diff

--- Server/obj/Debug/net8.0/server.AssemblyInfo.cs
+++ Server/obj/Debug/net8.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+deb09b860120b4157edc4a02ddce0a47e8a26cce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0908f5236288acaa09723d1020df6c9985c6333")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
